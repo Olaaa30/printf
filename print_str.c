1157210875d1b1ba57b311c4552429242474001b
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-08-09 09:18:35 -0700

    task 0

---
+++ print_str.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _puts- function to return number of string
+ * @str: string
+ * Return: number
+ */
+int _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return(i);
+}
+
+/**
+ * print_str - function that returns number of strings
+ * @args: arguments
+ * Return: result
+ */
+int print_str(va_list args)
+{
+	return(_puts(va_arg(args, char *)));
+}
