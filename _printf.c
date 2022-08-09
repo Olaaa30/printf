fea76ac04f708a3c7e1c7e2be75238fe8b5497c4
olayinka <adebisiolayinka30@gmail.com>
2022-08-09 00:04:48 -0700

    create base files

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdarg.h"
+/**
+ * _printf - prints chaars and strings to stcout
+ * @format: format
+ * Return: 0
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
